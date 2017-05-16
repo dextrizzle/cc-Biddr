38e4dea45b2fb0dff0f81bfefed8c3a52a3e1caa
dextrizzle <alfonzo_01@hotmail.com>
2017-05-15 21:43:40 -0700

    rewrite auctions controller rspec

--- spec/controllers/auctions_controller_spec.rb
+++ spec/controllers/auctions_controller_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe AuctionsController, type: :controller do
-  describe '#new' do
-  before { controller.stub(:authenticate_user!).and_return true }
+  before(:each) do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    # auction = FactoryGirl.create(:auction, user: user)
+  end
 
+  # let(:auction) { FactoryGirl.create(:auction, { user: FactoryGirl.create(:user) }) }
+
+  describe '#new' do
     before do
       get :new
     end
@@ -13,28 +19,41 @@ RSpec.describe AuctionsController, type: :controller do
     it 'instantiates a new auction object' do
       expect(assigns(:auction)).to be_a_new(Auction)
     end
-
     it 'renders the #new template' do
       expect(response).to render_template(:new)
     end
 
   end
   describe '#index' do
-    before { controller.stub(:authenticate_user!).and_return true }
-    
-    let(:auction) { FactoryGirl.create(:auction) }
-
-    it 'gets a HTTP 302 response' do
+    before do
       get :index
-      expect(response).to have_http_status(302)
     end
-    it 'renders index template' do
-      get :index
-      expect(response).to render_template(user_session_path)
+
+    it 'responds with HTTP 200 status' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the #index template' do
+      expect(response).to render_template(:index)
     end
   end
   describe '#create' do
-    before { controller.stub(:authenticate_user!).and_return true }
+    context 'with valid params' do
+      def valid_auction_params
+        post :create, params: { auction: FactoryGirl.attributes_for(:auction) }
+      end
+
+      it 'creates a new auction in the database' do
+        expect do
+          valid_auction_params
+        end.to change { Auction.count }.by(1)
+      end
+
+      it 'redirects to auction#show' do
+        valid_auction_params
+        expect(response).to redirect_to(auction_path(Auction.last))
+      end
+    end
 
     context 'with invalid params' do
       def invalid_auction_params
@@ -47,10 +66,22 @@ RSpec.describe AuctionsController, type: :controller do
         end.to_not change { Auction.count }
       end
 
-      it 'renders auction#new' do
+      it 'renders #new template' do
         invalid_auction_params
         expect(response).to render_template(:new)
       end
+    end
+  end
+  describe '#show' do
+    before do
+      post :create, params: { auction: FactoryGirl.attributes_for(:auction) }
+      get :show, params: { id: Auction.last.id }
+    end
+    it 'instantiates an auction object with id' do
+      expect(assigns(:auction)).to eq(Auction.last)
+    end
+    it 'renders the #show template' do
+      expect(response).to render_template(:show)
     end
   end
 end
--- spec/controllers/bids_controller_spec.rb
+++ spec/controllers/bids_controller_spec.rb
@@ -2,31 +2,9 @@ require 'rails_helper'
 
 RSpec.describe BidsController, type: :controller do
   describe '#create' do
-    before { controller.stub(:authenticate_user!).and_return true }
-      # @auction = FactoryGirl.create(:auction)
-      let(:user) { FactoryGirl.create(:user)}
-      let(:auction) { FactoryGirl.create(:auction, user_id: user.id) }
-    # let(:user) { FactoryGirl.create(:user)}
-    # let(:auction) { FactoryGirl.create(:auction, user_id: :user.id) }
 
-
-
-    context 'with invalid params' do
-      def invalid_bid_params
-        post :create, params: { auction_id: auction.id, bid: FactoryGirl.attributes_for(:bid, amount:nil) }
-      end
-
-      it 'doesn\'t create a bid in the database' do
-        expect do
-          invalid_bid_params
-        end.to_not change { Bid.count }
-      end
-
-      it 'shows the auction page' do
-        invalid_bid_params
-        expect(response).to render_template 'auctions/show'
-      end
-    end
+  end
+  describe '#index' do
 
   end
 end
